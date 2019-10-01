1ba63d75b8052300aa06c629e9ee898912c053cc
Alex <agent150891@gmail.com>
2019-10-01 17:50:20 +0300

    change structure

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -7,7 +7,8 @@ import {RequestMode} from '../proto/com/freeje/common_pb';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {PROFILE} from '../_interface/profile.interface';
 import {Router} from '@angular/router';
-
+import {grpc} from '@improbable-eng/grpc-web';
+import {makeDefaultTransport} from '@improbable-eng/grpc-web/dist/typings/transports/Transport';
 
 @Injectable()
 export class AuthService {
@@ -15,46 +16,72 @@ export class AuthService {
     firebase1: any = firebase;
     autorize = false;
     profile: any = new BehaviorSubject({});
+    req: any = new RequestMode();
+    metadataOuter: any = new grpc.Metadata();
+
     constructor(
         public afAuth: AngularFireAuth,
         private router: Router,
         private ngZone: NgZone
     ) {
-        this.client = new ProfileClient('https://webapi.freeje.org');
+        // this.req.setId(TokenService.getToken());
+        this.metadataOuter.set('jwt', TokenService.getToken());
+        this.client = new ProfileClient('https://webapi.freeje.org', {
+            transport: (options) => {
+                const t = grpc.CrossBrowserHttpTransport({withCredentials: true})(options);
+                t.start = function(metadata) {
+                    const token = TokenService.getToken();
+                    if (token) {
+                        metadata.set('JWT', token);
+                        this.metadata = metadata;
+                    }
+                };
+                return t;
+            },
+            debug: false,
+        });
     }
 
     logout() {
         TokenService.destroyToken();
-        this.ngZone.run( () => this.router.navigateByUrl(`login`));
+        this.ngZone.run(() => this.router.navigateByUrl(`login`));
     }
 
-    getProfileWithServer(token) {
+    getProfileWithServer() {
         return new Observable(obs => {
-            const req = new RequestMode();
-            // req.setId(token);
-            const streem = this.client.self(req, {'JWT': token});
+
+            const streem = this.client.self(this.req);
+
             streem.on('status', (status: any) => {
-                console.log('ApiService.getStream.status', status);
+                console.log('ApiService.getStream.status');
             });
             streem.on('data', (status: any) => {
+                console.log('STREAM DATA');
                 obs.next(status);
             });
-            streem.on('end', (status: any) => {
+            streem.on('end', () => {
+                console.log('STREAM END');
                 obs.complete();
             });
         })
-        .toPromise()
-        .then((res: any) => {
-            this.profile.next(this.reformarArrayInObject(res.array));
-            this.ngZone.run( () => this.router.navigateByUrl(`profile`));
-        });
+            .toPromise()
+            .then((res: any) => {
+                if (!res) {
+                    // TokenService.destroyToken();
+                    return this.TOKEN_REFRESH_LOGIN();
+                }
+                this.profile.next(this.reformarArrayInObject(res.array));
+                this.ngZone.run(() => this.router.navigateByUrl(`profile`));
+            });
     }
 
     isAutorize() {
         const token = TokenService.getToken();
-        if (this.autorize) return;
+        if (this.autorize) {
+            return;
+        }
         if (token) {
-            this.getProfileWithServer(token);
+            this.getProfileWithServer();
         } else {
             this.router.navigateByUrl(`login`);
         }
@@ -70,12 +97,26 @@ export class AuthService {
         provider.addScope('email');
         this.afAuth.auth.signInWithPopup(provider)
             .then((res: any) => {
+                console.log('loginWithFirebase');
                 TokenService.saveToken(res.user._lat);
                 this.autorize = true;
-                return this.getProfileWithServer(TokenService.getToken());
+                return this.getProfileWithServer();
             })
             .catch((error) => console.log(error));
 
+    }
+
+    async TOKEN_REFRESH_LOGIN() {
+        const auth = await firebase.auth();
+        this.getCurrentUser(auth)
+            .then((res: any) => res.getIdToken(true))
+            .then((idToken: string) =>  {
+                TokenService.saveToken(idToken);
+                return this.getProfileWithServer();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     }
 
     private reformarArrayInObject(array) {
@@ -83,5 +124,13 @@ export class AuthService {
         Object.keys(obj).forEach((item, index) => obj[item] = array[index]);
         return obj;
     }
-}
 
+     getCurrentUser(auth) {
+        return new Promise((resolve, reject) => {
+            const unsubscribe = auth.onAuthStateChanged(user => {
+                unsubscribe();
+                resolve(user);
+            }, reject);
+        });
+    }
+}
